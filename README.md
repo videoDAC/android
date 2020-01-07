0a70ce5703cc42567cc339031ace5a1994fceef6
mulili <mulili.nzuki@gmail.com>
2020-01-07 16:09:35 +0300

    initial app commits

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# apps
+# Intro
 
-This repo for apps.
+This repo contains the native kotlin and swift projects for the `Video Dac` app.
 
-Smartphone apps, Tablet apps, TV apps, SmartWatch apps, Dumbphone apps, Desktop apps, IOT apps, Car apps...
 
-But we'll start with Smartphone apps.
+# Building
+
+To build out the app for the both android and iOS, please do the following,
+
+### Android
+
+1. Open the `android` project in [Android Studio](https://developer.android.com/studio)
+2. Let the project finish syncing gradle and update any dependencies in the project as prompted.
+3. Finally connect your device or start up an emulator and deploy the app to it by going to the menu `Run -> Run 'app'`
+
+To modify the `STREAM_URL` open the file `MainActivity.kt`  and modify line 35 by entering the new valid hls URL. Then build the project as explained above and deploy the app.
+
+### iOS
+
+1. Open the `ios` project in [Xcode](https://developer.apple.com/xcode/)
+2. Build the project by going to the menu `Product -> Vuild`
+3. Finally deploy click on the `play button` to deploy the app to the simulator or a [provisioned](https://developer.apple.com/library/archive/documentation/ToolsLanguages/Conceptual/YourFirstAppStoreSubmission/ProvisionYourDevicesforDevelopment/ProvisionYourDevicesforDevelopment.html) iOS device
+
+To modify the `STREAM_URL` open the file `ViewcController.swift`  and modify line 17 by entering the new valid hls URL. Then build the project as explained above and deploy the app.
---
+++ android/app/src/main/java/com/videodac/hls/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.videodac.hls
+
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import android.widget.RelativeLayout
+
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.video.VideoListener
+
+import kotlinx.android.synthetic.main.video_layout.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var player: SimpleExoPlayer
+    private lateinit var mediaDataSourceFactory: DataSource.Factory
+    private var fullscreen = false
+
+    companion object {
+        const val STREAM_URL = "http://159.100.251.158:8935/stream/0xdac817294c0c87ca4fa1895ef4b972eade99f2fd.m3u8"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.video_layout)
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(this)
+        mediaDataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "video_dac"))
+
+        val mediaSource = HlsMediaSource.Factory(mediaDataSourceFactory).createMediaSource(Uri.parse(STREAM_URL))
+
+        with(player, {
+            prepare(mediaSource, false, false)
+            playWhenReady = true
+            addVideoListener(object : VideoListener {
+                override fun onVideoSizeChanged(width: Int, height: Int, unappliedRotationDegrees: Int, pixelWidthHeightRatio: Float) {
+                    initView.visibility = View.GONE
+                    Log.d("VIDEO_DAC", String.format("play video listener video size changed, WIDTH IS %1\$2s , HEIGHT IS %2\$2s", width.toString(), height.toString()))
+
+                    when {
+                        width > height -> goFullScreen("landscape")
+                        width <= height -> goFullScreen("portrait")
+                    }
+                }
+
+                override fun onRenderedFirstFrame() {
+                    Log.d("VIDEO_DAC", String.format("First frame rendered"))
+                }
+            })
+
+        })
+
+        playerView.setShutterBackgroundColor(Color.TRANSPARENT)
+        playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+        playerView.player = player
+        playerView.requestFocus()
+
+        with(playerView, {
+            setOnClickListener {
+                finish()
+            }
+        })
+
+    }
+
+    @SuppressLint("DefaultLocale", "InlinedApi")
+    private fun goFullScreen(orientation:String) {
+
+        if ( orientation.toLowerCase() == "portrait" ) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
+        if ( orientation.toLowerCase() == "landscape" ) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        val params = playerView.layoutParams as RelativeLayout.LayoutParams
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        playerView.layoutParams = params
+        fullscreen = true
+    }
+
+    private fun releasePlayer() {
+        player.release()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        initializePlayer()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+}
